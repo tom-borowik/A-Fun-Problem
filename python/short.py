eaec685afe94bcb08ea5ffe7eeefe9fc61e57855
Tom Borowik <tom.borowik@tafe.qld.edu.au>
2015-06-14 14:18:10 +1000

    Converted tabs to spaces

--- python/short.py
+++ python/short.py
@@ -1,5 +1,5 @@
 def aSillyFunctionShort(list):
-	'''A function that will sort a list of dictionaries and group 
-	them alphabetically by brand then type'''
-	#Most compact solution
+    '''A function that will sort a list of dictionaries and group 
+    them alphabetically by brand then type'''
+    #Most compact solution
     return sorted(list, key=lambda k:(k['brand'], k['type']))
